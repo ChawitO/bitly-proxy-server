888ed4c775bcb4ccccf1432a42c9a01b8f1f2c6b
Chawit.O <chawit.ocr@gmail.com>
2021-09-23 19:54:53 +0700

    Setup proxy to bit.ly with express.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import axios from 'axios'
 
-console.log('Node.js with ever running nodemon!');
-console.log(process.env.SAMPLE_SECRET);
+const app = express()
+const PORT = process.env.PORT
+const config = { headers: { 'Authorization': `Bearer ${process.env.BITLY_KEY}` } }
+
+app.use(cors())
+app.use(express.json())
+
+app.post('/api/shorten', (req, res) => {
+  axios.post('https://api-ssl.bitly.com/v4/shorten', { long_url: req.body.longUrl }, config )
+    .then(({ data }) => res.json(data.id))
+    .catch(err => console.log(err))
+})
+
+app.listen(PORT, () =>
+  console.log(`url-shortener-proxy-server is listening on port ${PORT}!`)
+)
